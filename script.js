58cf0f6182b95dd579ecc8b9a244d7b5e33c6d26
supritkumar shah <suprit7@gmail.com>
2016-06-08 20:06:18 -0400

    html, css and simon game board designed with rough qjquery rough draft

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  console.log("connected to Javascript file");
+
+
+});
+
+
+
+/*
+Rough draft
+1. function to flash colors in a pattern
+    -variable of type array to store the above pattern
+2. flashing color function
+3. game start object
+4. reset game
+5. start game on play button function
+6. UserClick on colors
+  - storing those clicks and comparing #1
+7. removing all events from all elements after each play
+
+
+
+*/
